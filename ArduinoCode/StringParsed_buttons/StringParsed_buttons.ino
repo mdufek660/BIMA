3fcfcb8a3b39b6a6fe8f643460f9cef53221a23c
mdufek660 <mdufek.660@gmail.com>
2019-04-08 13:21:37 -0400

    Update StringParsed_buttons.ino

--- ArduinoCode/StringParsed_buttons/StringParsed_buttons.ino
+++ ArduinoCode/StringParsed_buttons/StringParsed_buttons.ino
@@ -5,8 +5,7 @@ Note this code loops automatically with a short break between scrolls, future ve
 Special thanks to Pizzle on Discord for his help in troubleshooting a previous version of the project~
 Note I tried to do the code with various libraries being made but could not fix it. Will try to update that in later
 */
-int pin=0;
-    char dictionary[50][9]= //Please note if you change the first value, you MUST change j to match minus 1
+  char dictionary[50][9]= //Please note if you change the first value, you MUST change j to match minus 1
     {
       "A 100000", "B 101000", "C 110000", "D 110100", "E 100100", "F 111000", "G 111100", "H 101100", "I 011000", "J 011100", "K 100010",
       "L 101010", "M 110010", "N 110110", "O 100110", "P 111010", "Q 111110", "R 101110", "S 011010", "T 011110", "U 100011", "V 101011",
@@ -14,22 +13,24 @@ int pin=0;
       "8 101100", "9 011000", "0 011100", ". 001101", ", 001000", ", 001000", "? 001000", "; 001010", "! 001110", "\" 001011", "- 000011",
       "' 000010", "{ 001111", "* 010111", "+ 000001"
     };
+    
   int j=49; //Note this MUST match the size of the dictionary entries minus 1! Larger means you will run out of the array bounds, smaller means you wont search each entry!
   int i=0;
   int k=2;
   int delayValue=500;
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, INPUT);
-  pinMode(10, INPUT);
+  String hello = "Hello there!";
   
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600);//makes the arduino able to output to the console in the topright of the screen.
+  
+  pinMode(3, OUTPUT);//Reset for the cells
+  pinMode(4, OUTPUT);//A on the 1-to-8 demultiplexor
+  pinMode(5, OUTPUT);//B on the 1-to-8 demultiplexor
+  pinMode(6, OUTPUT);//C on the 1-to-8 demultiplexor
+  pinMode(7, OUTPUT);//Outputs to cell 1
+  pinMode(8, OUTPUT);//Outputs to cell 2
+  pinMode(9, INPUT);//input to move backwards
+  pinMode(10, INPUT);//input to move forwards
 }
 
 
@@ -37,31 +38,30 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
- int  n=0;
-  String hello = "Hello there!";
-  while(n<hello.length())
+  
+  int  n=0;
+  while(n<hello.length()) //ensure we don't run out of bounds on the string
   {
-    if(isUpperCase(hello[n]))
+    if(isUpperCase(hello[n])) //check if I need the uppercase cell
     {
       Serial.println("Printing cap letter");
-      makeBraille('+', hello[n]);
+      makeBraille('+', hello[n]); //Send the braille cells for capital and the char
       n++;
      Serial.println(n);
     }
-    if(isDigit(hello[n]))
+    if(isDigit(hello[n]))//check if i need the digit cell
     {
       Serial.println("Printing number");
-      makeBraille('*', hello[n]);
+      makeBraille('*', hello[n]); //Send the braille cells for number and the char
       n++;
       Serial.println(n);
     }
-    if(n<(hello.length()-1) && !(isUpperCase(hello[n])) && !(isDigit(hello[n])))
+    if(n<(hello.length()-1) && !(isUpperCase(hello[n])) && !(isDigit(hello[n]))) //Checks if the character is in bounds and isn't a number or caps. probably redundant but not near the program byte cap
     {
-      if(!(isUpperCase(hello[n+1])) && !(isDigit(hello[n+1])))
+      if(!(isUpperCase(hello[n+1])) && !(isDigit(hello[n+1]))) //Checks if the char after the current wouldn't require two cells (ie caps or number)
       {
       Serial.println("Printing 2 letters");
-        makeBraille(hello[n], hello[n+1]);
+        makeBraille(hello[n], hello[n+1]); //Print the current char and the next
         n++;
         n++;
         Serial.println(n);
@@ -69,12 +69,12 @@ void loop() {
       else
       {
       Serial.println("Printing 1 letter and hyphon");
-        makeBraille(hello[n], '-');
+        makeBraille(hello[n], '-'); //Prints the current letter and the hyphon so you know the word is continued next but couldn't fit
         n++;
         Serial.println(n);
       }
     }
-    if(n==(hello.length()-1) && !(isUpperCase(hello[n])) && !(isDigit(hello[n])))
+    if(n==(hello.length()-1) && !(isUpperCase(hello[n])) && !(isDigit(hello[n]))) //checks to see if it is the end of the string and if so prints the last char and a space
     {
       Serial.println("Printing one letter and space");
       makeBraille(hello[n], ' ');
@@ -83,9 +83,9 @@ void loop() {
     }
     
     delay(2000);
-    pinMode(3, OUTPUT);
+    pinMode(3, OUTPUT); //clear the cells with the next four lines
     digitalWrite(3,1);
-    delay(100);
+    delay(10);
     digitalWrite(3,0);
   }
 }
@@ -114,9 +114,10 @@ void makeBraille(char c, char d){
   Serial.println(i);
   */
 
-  c = charUpper(c);
+  c = charUpper(c); //using char upper to search the array
   d = charUpper(d);
-  
+
+  //The following four lines are for diagnostic purposes
   Serial.print("Inside the makeBraille function, value is: ");
   Serial.print(c);
   Serial.print(" and ");
@@ -125,23 +126,24 @@ void makeBraille(char c, char d){
   //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Get first value
   if( c != ' ') //set up the first value
   {
-  while(i<j && found==false)
+  while(i<j && found==false) //checks to see if it has found the char while also ensuring it doesn't run out of bounds if it hasn't.
   {
     
-    if(dictionary[i][0]==c)
+    if(dictionary[i][0]==c) //check if the current location in the dictionary matches the current char
     {
       //Serial.println("Found letter: " + c);
-       while(k<8)
+       while(k<8) //this while loop assembles a string from the char array to parse out later
        {
-        value1 += dictionary[i][k];
+        value1 += dictionary[i][k]; 
         k++;
         found = true;
        }
      Serial.println(value1);
     }
     i++;
-  }
-  if(found == false)
+  }//end the while.
+  
+  if(found == false) //Couldn't find it
   {
     Serial.println("This was not the char we were looking for");
     value1 = "111111";//this is the default for a character not found in the dictionary, all bumps
@@ -156,7 +158,7 @@ void makeBraille(char c, char d){
   i=0;
   k=2;
   found=false;
-  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Get second value
+  //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Get second value, look at instructions for c
   if( d != ' ') //set up the first value
   {
   while(i<j && found==false)
@@ -192,7 +194,7 @@ void makeBraille(char c, char d){
   digitalWrite(5,0);//Sets B
   digitalWrite(6,0);//Sets C
   delay(delayValue);
-  if(value1[l]=='0'){digitalWrite(7,0);}
+  if(value1[l]=='0'){digitalWrite(7,0);} //this if-else controls whether the current cell location is a 1 or a 0 based on the string location
   else{digitalWrite(7,1);}
   if(value2[l]=='0'){digitalWrite(8,0);}
   else{digitalWrite(8,1);}
